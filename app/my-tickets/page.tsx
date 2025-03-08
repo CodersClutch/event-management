e6ea420a0ed01070b744a67d94adef35016eb750
Amadou Jamanka <ajamanka72@gmail.com>
2025-03-08 11:55:12 +0000

    Add MyTickets page component with basic structure

---
+++ app/my-tickets/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        My 
+        <div>
+          Hello World 
+        </div>
+    </div>
+  )
+}
+
+export default page
