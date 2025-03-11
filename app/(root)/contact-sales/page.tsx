05cbdb0825f5c53f657b7a92bf3880413b7973b9
Amadou Jamanka <ajamanka72@gmail.com>
2025-03-11 17:05:52 +0000

    Add sponsor logos section to Contact Sales page for enhanced branding

--- app/(root)/contact-sales/page.tsx
+++ app/(root)/contact-sales/page.tsx
@@ -1,7 +1,15 @@
 import ContactTicketsSales from '@/components/nav-links/contact-sales/ContactTicketsSales'
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
+  const sponsors = [
+    {name: 'Coca-Cola', logo: 'https://eventbrite-s3.s3.us-east-1.amazonaws.com/marketing/landingpages/assets/2025/rebrand/l/contact-eventbrite-sales/trusted-by/a--logo-us-001.png'},
+    {name: 'Pepsi', logo: 'https://eventbrite-s3.s3.us-east-1.amazonaws.com/marketing/landingpages/assets/2025/rebrand/l/contact-eventbrite-sales/trusted-by/a--logo-us-002.png'},
+    {name: 'Nike', logo: 'https://eventbrite-s3.s3.us-east-1.amazonaws.com/marketing/landingpages/assets/2025/rebrand/l/contact-eventbrite-sales/trusted-by/a--logo-us-003.png'},
+    {name: 'Adidas', logo: 'https://eventbrite-s3.s3.us-east-1.amazonaws.com/marketing/landingpages/assets/2025/rebrand/l/contact-eventbrite-sales/trusted-by/a--logo-us-004.png'},
+    {name: 'Apple', logo: 'https://eventbrite-s3.s3.us-east-1.amazonaws.com/marketing/landingpages/assets/2025/rebrand/l/contact-eventbrite-sales/trusted-by/a--logo-us-005.png'},
+  ]
   return (
     <div className='px-4 lg:px-16'>
 
@@ -26,7 +34,18 @@ const page = () => {
 
       {/* Sponsor */}
       <div>
-
+      <div className='flex justify-center items-center flex-col py-24 gap-5'>
+        <div>
+          <h1 className='font-bold text-6xl'>Trusted by</h1>
+        </div>
+        <div className='flex flex-wrap justify-center items-center gap-5'>
+        {sponsors.map((sponsor, index) => (
+          <div key={index} className='py-5 flex-1'>
+            <Image src={sponsor.logo} alt={sponsor.name} width={200} height={100} />
+          </div>
+        ))}
+        </div>
+      </div>
       </div>
     </div>
   )
