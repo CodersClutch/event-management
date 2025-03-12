06c4881a56b404ad8fe017636b9cc4b1d4910587
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-03-12 09:51:16 +0000

    Add RefundInfo component to Help Center and integrate it into the 4kiddo-charge page

--- app/(root)/help-center/4kiddo-charge/page.tsx
+++ app/(root)/help-center/4kiddo-charge/page.tsx
@@ -1,3 +1,4 @@
+import RefundInfo from '@/components/nav-links/Help-center/RefundInfo'
 import TicketBanner from '@/components/nav-links/Help-center/TicketBanner'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
     <div className='pt-[8%] px-[3.5%]'>
         <TicketBanner title="Request a refund" />
+        <RefundInfo />
     </div>
   )
 }
