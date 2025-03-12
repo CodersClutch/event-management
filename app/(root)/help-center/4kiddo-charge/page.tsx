c4a5b2ba655f1b635a104a1b37812298d9c8909a
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-03-12 09:57:51 +0000

    new component creation

--- app/(root)/help-center/4kiddo-charge/page.tsx
+++ app/(root)/help-center/4kiddo-charge/page.tsx
@@ -1,3 +1,4 @@
+import Questions from '@/components/nav-links/Help-center/Questions'
 import RefundInfo from '@/components/nav-links/Help-center/RefundInfo'
 import TicketBanner from '@/components/nav-links/Help-center/TicketBanner'
 import React from 'react'
@@ -7,6 +8,7 @@ const page = () => {
     <div className='pt-[8%] px-[3.5%]'>
         <TicketBanner title="Request a refund" />
         <RefundInfo />
+        <Questions  />
     </div>
   )
 }
---
+++ app/(root)/help-center/contact-the-event-organizer/page.tsx
@@ -0,0 +1,12 @@
+import Questions from '@/components/nav-links/Help-center/Questions'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div  className='pt-[8%] px-[3.5%]'>
+        <Questions  />
+    </div>
+  )
+}
+
+export default page
